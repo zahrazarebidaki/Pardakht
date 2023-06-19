02aef333a5e2d00c6706ee14178a5b3b7ce8afaf
Zahra <zhrazareofficial@gmail.com>
2023-06-19 20:27:58 +0330

    add retrofit lib

---
+++ app/src/main/java/ir/zhra_ze/pardakht/ApiService.java
@@ -0,0 +1,18 @@
+package ir.zhra_ze.pardakht;
+
+import com.google.gson.JsonObject;
+
+import org.json.JSONObject;
+
+import ir.zhra_ze.pardakht.model.fixedline.FixedLineNumber;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface ApiService {
+    @POST("Inquiry/FixedLineBillInquiry")
+    Call<FixedLineNumber> getInquiry(@Body JsonObject phoneNumber);
+}
